b0005132896bc3757dabb4cb804de552ff538b81
SantiagoCon <56417000+SantiagoCon@users.noreply.github.com>
2022-12-26 18:51:15 -0600

    Update script.js

--- client/script.js
+++ client/script.js
@@ -70,7 +70,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv);
 
-    const response = await fetch('http://localhost:3000', {
+    const response = await fetch('https://centauros.baak-mexico.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
